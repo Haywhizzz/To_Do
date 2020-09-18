9e4aa42a84748343a10f3de8dd89b3086fab844a
haywhizzz <haywhizzz@gmail.com>
2020-09-18 13:41:54 +0100

    Make model routes and controller for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
   get 'welcome/index'
   root 'welcome#index'
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
